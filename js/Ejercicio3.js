23c320ef087c18e337e38d25940b8a8ddd3d6d17
Your Nameedwarrojas26 <edrojas843@misena.edu.co>
2022-09-08 10:45:25 -0500

    Avances2

--- js/Ejercicio3.js
+++ js/Ejercicio3.js
@@ -4,13 +4,14 @@ let lado3 = document.querySelector(".lado3");
 let consultar = document.querySelector(".consultar");
 
 function clasificacionTriangulos() {
-  if ((lado1.value = lado2.value = lado3.value)) {
-    alert(
-      `los 3 lados son iguales, por tal motivo se trata de un triángulo equilatero`
-    );
-  } else if ((lado1.value != lado2.value) != lado3.value) {
+  //
+  if (lado1 != lado2 && lado1 != lado3 && lado2 != lado3) {
     alert(
       `los 3 lados son diferentes, por tal motivo se trata de un triángulo escaleno`
+    );
+  } else if ((lado1 = lado2 = lado3)) {
+    alert(
+      `los 3 lados son iguales, por tal motivo se trata de un triángulo equilatero`
     );
   }
 }
